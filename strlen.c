72fdd86872a61f85ebdfe1c558ac3aa4cee25a05
Les Smith <lestsmith@icloud.com>
2021-04-15 19:24:59 +0200

    new

---
+++ strlen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+    char str1[10] = "first";
+
+    printf ("The length of the string %s is %d\n",
+      str1, strlen(str1));
+
+    return 0;  
+}
