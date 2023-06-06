45057fc05de2fb43684ba46f18328099c6ea3207
Les Smith <lestsmith@icloud.com>
2023-06-06 21:35:57 +0200

    changes to help understand the code

--- compareStrings.c
+++ compareStrings.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-void main (void)
+int main (void)
 {
   char str1[10] = "first";
-  char str2[10] = "fire";
+  char str2[10] = "first";
   char *ptr1 = str1, *ptr2 = str2;
 
   while (*ptr1 != 0 && *ptr2 != 0)
   {
+    printf("\n %s", ptr1);
     if (*ptr1 != *ptr2)
     {
       break;
@@ -24,4 +25,5 @@ void main (void)
   {
     printf ("The two strings are different.\n");
   }
+  return 0;
 }
