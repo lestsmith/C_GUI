6839a6c6a7e738bbcd9a2eed522ffcd382abf5a0
Les Smith <lestsmith@icloud.com>
2023-06-06 21:36:11 +0200

    new

---
+++ numLetters.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void){
+
+  char letter[1] = "";
+  char word[30] = "";
+
+  int count = 0;
+  int i = 0;
+
+  printf("Enter a word: ");
+  scanf("%s", word);
+
+  printf("\nEnter a letter: ");
+  scanf("%s", letter);
+
+  for (i = 0; i < 30; i++){
+    //printf(" %c", letter[0]);
+    if (word[i] == letter[0]){
+      count++;
+    }
+  }
+
+  printf("\n%i\n", count);
+
+  return 0;
+}
