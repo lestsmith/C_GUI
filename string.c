3b05ebecbbed9b0e41940249a89e64fb6f50565c
Les Smith <lestsmith@icloud.com>
2023-06-19 21:50:36 +0200

    Change main to return int

--- string.c
+++ string.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void main (void)
+int main (void)
 {
  char str1[10] = "first";
  char str2[10] = "second";
@@ -22,4 +22,7 @@ void main (void)
 }
  *dst = 0;
  printf ("%s + %s = %s\n", str1, str2, str3);
+
+  return 0;
+
 }
