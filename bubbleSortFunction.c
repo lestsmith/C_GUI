b7708a5c8679fbf8b71202df7764e266913c1e63
LesTSmith <lestsmith@googlemail.com>
2023-06-23 09:14:09 +0200

    deleted & minor change

--- bubbleSortFunction.c
+++ bubbleSortFunction.c
@@ -3,7 +3,7 @@
 void bubbleSort(int myNumbers[], int len);
 
 int main() {
-  int myNumbers[] = {30, 50, 20, 40, 10, 60}; 
+  int myNumbers[] = {30, 50, 55, 20, 40, 10, 60}; 
   int len = sizeof(myNumbers) / sizeof(myNumbers[0]);
   int arrayList[len];
   printf("%d\n\n", len);
